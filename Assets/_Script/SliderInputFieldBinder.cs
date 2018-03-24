a1829324db3013f21667ef8e7e8ada6e2efedec9
Taranjit Singh <taranjit.singh@studenti.unimi.it>
2018-03-24 19:38:02 +0100

    GUI improved

---
+++ Assets/_Script/SliderInputFieldBinder.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class SliderInputFieldBinder : MonoBehaviour {
+
+    public Slider slider;
+    public InputField input;
+
+    private float min;
+    private float max;
+
+
+    bool ignoreChange = false;
+
+
+    private void Start()
+    {
+        min = slider.minValue;
+        max = slider.maxValue;
+        input.text = slider.value.ToString();
+    }
+
+
+
+    public void InputFieldChanged()
+    {
+        string text = input.text;
+        float number;
+        bool isNumber = float.TryParse(text, out number);
+        if (isNumber)
+        {
+            if (number >= max)
+            {
+                input.text = max.ToString();
+                slider.value = max;
+            }
+            else if (number <= min)
+            {
+                input.text = min.ToString();
+                slider.value = min;
+            }
+            else
+                slider.value = number;
+        }
+        else
+        {
+            if (text == "")
+            {
+                ignoreChange = true;
+                slider.value = min;
+            }
+               
+            else
+                input.text = slider.value.ToString();
+        }
+        
+    }
+    public void SliderChanged()
+    {
+        if (!ignoreChange)
+            input.text = slider.value.ToString();
+        else
+            ignoreChange = false;
+    }
+
+}
