63b5dcc68bfe65e6d54e052f57a7f20119a9aefd
Taranjit Singh <taranjit.singh@studenti.unimi.it>
2018-04-17 12:09:51 +0200

    Started tapping level

---
+++ Assets/_Script/Tapping/Button.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+
+    private bool inTime = false;
+    public Song song;
+    public float errorPercentage = 0.3f;
+
+    public void Clicked()
+    {
+        if (inTime)
+            StartCoroutine(ChangeColor(Color.green));
+        else
+            StartCoroutine(ChangeColor(Color.red));
+    }
+
+    IEnumerator ChangeColor(Color color)
+    {
+        SpriteRenderer sp = GetComponent<SpriteRenderer>();
+        Color old = sp.color;
+        sp.color = color;
+        yield return new WaitForSeconds(1);
+        sp.color = old;
+    }
+
+    IEnumerator Loop() { 
+
+        yield return new WaitForSeconds(song.offset - song.tempo * errorPercentage);
+        inTime = true;
+
+        yield return new WaitForSeconds(2*song.tempo * errorPercentage);
+        inTime = false;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(song.tempo - 2 * song.tempo * errorPercentage);
+            inTime = true;
+            yield return new WaitForSeconds(2*song.tempo * errorPercentage);
+            inTime = false;
+        }
+    }
+
+
+}
---
+++ Assets/_Script/Tapping/Tap.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour {
+    Plane objPlane;
+    public bool useMouse = false;
+
+    public Button button;
+
+    void Start()
+    {
+        objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
+        transform.position = new Vector3();
+    }
+
+    void Update()
+    {
+        if (ThereIsInput() && InputStarted())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(GetInputPosition());
+            float distance;
+            if (objPlane.Raycast(ray, out distance))
+            {
+                this.transform.position = ray.GetPoint(distance);
+                button.Clicked();
+            }
+                
+        }
+    }
+
+    private bool ThereIsInput()
+    {
+        if (useMouse)
+            return Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0);
+        else
+            return Input.touchCount > 0;
+    }
+
+    private Vector3 GetInputPosition()
+    {
+        if (useMouse)
+            return Input.mousePosition;
+        else
+            return (Input.GetTouch(0).position);
+    }
+    private bool InputStarted()
+    {
+        if (useMouse)
+            return Input.GetMouseButtonDown(0);
+        else
+            return Input.GetTouch(0).phase == TouchPhase.Began;
+    }
+
+    private bool InputOngoing()
+    {
+        if (useMouse)
+            return Input.GetMouseButton(0);
+        else
+            return Input.GetTouch(0).phase == TouchPhase.Moved;
+    }
+
+    private bool InputEnded()
+    {
+        if (useMouse)
+            return Input.GetMouseButtonUp(0);
+        else
+            return (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled);
+    }
+}
