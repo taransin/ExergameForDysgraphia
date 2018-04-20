2d060f6866fb2b8dcce0dfa3fbfa57f7902b7017
Taranjit Singh <taranjit.singh@studenti.unimi.it>
2018-04-20 11:24:34 +0200

    created isochrony level

---
+++ Assets/_Script/Isochrony/CircleTouch.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleTouch : MonoBehaviour {
+
+    public GameObject point;
+    private GameObject instance;
+    Plane objPlane;
+
+    public bool useMouse = false;
+
+
+    void Start()
+    {
+        objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
+    }
+
+    void Update()
+    {
+        if (ThereIsInput())
+        {
+            if (InputStarted())
+            {
+                Ray ray = Camera.main.ScreenPointToRay(GetInputPosition());
+                float distance;
+                if (objPlane.Raycast(ray, out distance))
+                    instance = Instantiate(point, ray.GetPoint(distance), Quaternion.identity);
+            }
+            else if (InputOngoing())
+            {
+                Ray ray = Camera.main.ScreenPointToRay(GetInputPosition());
+                float distance;
+                if (objPlane.Raycast(ray, out distance))
+                    instance.transform.position = ray.GetPoint(distance);
+            }
+            else if (InputEnded() && instance)
+            {
+                instance.GetComponent<Point>().KillPoints();
+
+            }
+        }
+    }
+
+
+    private bool ThereIsInput()
+    {
+        if (useMouse)
+            return Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0);
+        else
+            return Input.touchCount > 0;
+    }
+
+    private Vector3 GetInputPosition()
+    {
+        if (useMouse)
+            return Input.mousePosition;
+        else
+            return (Input.GetTouch(0).position);
+    }
+    private bool InputStarted()
+    {
+        if (useMouse)
+            return Input.GetMouseButtonDown(0);
+        else
+            return Input.GetTouch(0).phase == TouchPhase.Began;
+    }
+
+    private bool InputOngoing()
+    {
+        if (useMouse)
+            return Input.GetMouseButton(0);
+        else
+            return Input.GetTouch(0).phase == TouchPhase.Moved;
+    }
+
+    private bool InputEnded()
+    {
+        if (useMouse)
+            return Input.GetMouseButtonUp(0);
+        else
+            return (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled);
+    }
+}
---
+++ Assets/_Script/Isochrony/Point.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+
+    private bool active = true;
+    private bool inArea;
+    public string accettableAreaTag;
+
+
+    public void KillPoints()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Color c = sr.color;
+        c.a = 0;
+        sr.color = c;
+        active = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("triggered " + collision.tag);
+        if (collision.tag == accettableAreaTag)
+        {
+            inArea = true;
+            collision.GetComponent<Button>().Clicked();
+        }
+            
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (active && collision.tag == accettableAreaTag)
+        {
+            inArea = false;
+        }
+    }
+
+}
